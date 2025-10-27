03a33c8e862ab16bb1a8d155c138ec419608e92c
haryag <meckkakakaka@gmail.com>
2025-10-28 07:50:37 +0900

    Update login.js

--- static/login.js
+++ static/login.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCgDtdyFtxtLEMZ6Gt0_haDlpLFg5UYkBQ",
     authDomain: "studyplanner-12345.firebaseapp.com",
     projectId: "studyplanner-12345",
-    storageBucket: "studyplanner-12345.firebasestorage.app",
+    storageBucket: "studyplanner-12345.appspot.com",
     messagingSenderId: "11066629765",
     appId: "1:11066629765:web:04cd920de2a077a16713ec"
 };
@@ -65,4 +65,5 @@ onAuthStateChanged(auth, (user) => {
         status.textContent = "未ログイン";
     }
 });
+
 
