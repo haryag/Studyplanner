a2358c979afdea096253a5fb83134a9467f5fa0c
haryag <meckkakakaka@gmail.com>
2025-11-11 22:33:21 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 import { currentUser } from './login.js';
-import { getFirestore, doc, setDoc, getDoc } from './firebase/firebase-firestore.js';
+import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js';
 const db = getFirestore();
 
 // --- Service Worker ---
@@ -660,6 +660,7 @@ window.addEventListener('DOMContentLoaded', () => {
         renderTodayPlans();
     }, 0); // 0msでも次のイベントループに回るので初期表示は速い
 });
+
 
 
 
