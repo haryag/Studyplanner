b90477d98d554687fabc924247bc06411679ed7e
haryag <meckkakakaka@gmail.com>
2025-11-22 20:51:08 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/fireb
 const db = getFirestore();
 
 // --- Service Worker ---
-const SW_VERSION = 'v2.1.0';
+const SW_VERSION = 'v1.8.0';
 const BASE_PATH = '/Studyplanner/';
 
 // --- データ初期化 ---
