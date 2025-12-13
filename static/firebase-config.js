55e68144ed58ee71dd3752d0b0be315fd5729f19
Haryag <meckkakakaka@gmail.com>
2025-12-13 23:55:17 +0900

    Create firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,8 @@
+self.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCgDtdyFtxtLEMZ6Gt0_haDlpLFg5UYkBQ",
+    authDomain: "studyplanner-12345.firebaseapp.com",
+    projectId: "studyplanner-12345",
+    storageBucket: "studyplanner-12345.appspot.com",
+    messagingSenderId: "11066629765",
+    appId: "1:11066629765:web:04cd920de2a077a16713ec"
+};
